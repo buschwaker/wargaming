28aff33fa6738af163b0808c3f82d2c730927df6
Александр Крошко <akroshko1995@gmail.com>
2022-07-18 14:39:31 +0300

    unittests added

--- tests/test_sorts.py
+++ tests/test_sorts.py
@@ -20,14 +20,17 @@ class TestSorts(unittest.TestCase):
         call_sorted_desc = task3.quick_sort(self.sorted_desc)
 
         self.assertEqual(
-            call_random, self.result_random_list, 'Функция quick_sort() не работает со списком случайных значений'
+            call_random, self.result_random_list,
+            'Функция quick_sort() не работает со списком случайных значений'
         )
         self.assertEqual(
-            call_sorted_asc, self.result_sorted_asc, 'Функция quick_sort() не работает с сортированным списком'
+            call_sorted_asc, self.result_sorted_asc,
+            'Функция quick_sort() не работает с сортированным списком'
         )
         self.assertEqual(
             call_sorted_desc, self.result_sorted_desc,
-            'Функция quick_sort() не работает со списком случайных значений(от большего к меньшему)'
+            'Функция quick_sort() не работает со '
+            'списком случайных значений(от большего к меньшему)'
         )
 
     def test_merge_sort(self):
@@ -36,16 +39,19 @@ class TestSorts(unittest.TestCase):
         call_sorted_desc = task3.merge_sort(self.sorted_desc)
 
         self.assertEqual(
-            call_random, self.result_random_list, 'Функция merge_sort() не работает со списком случайных значений'
+            call_random, self.result_random_list,
+            'Функция merge_sort() не работает со списком случайных значений'
         )
         self.assertEqual(
-            call_sorted_asc, self.result_sorted_asc, 'Функция merge_sort() не работает с сортированным списком'
+            call_sorted_asc, self.result_sorted_asc,
+            'Функция merge_sort() не работает с сортированным списком'
         )
         self.assertEqual(
             call_sorted_desc, self.result_sorted_desc,
-            'Функция merge_sort() не работает со списком случайных значений(от большего к меньшему)'
+            'Функция merge_sort() не работает со '
+            'списком случайных значений(от большего к меньшему)'
         )
 
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
